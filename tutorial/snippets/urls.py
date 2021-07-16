9b6b517a1a5f398b70c66ac4005d08d59a2728b4
testuser <neo.aoi.takahashi@gmail.com>
2021-07-16 15:45:54 +0900

    Django REST Framework - 1to6

---
+++ tutorial/snippets/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets import views
+# API endpoints
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+]
